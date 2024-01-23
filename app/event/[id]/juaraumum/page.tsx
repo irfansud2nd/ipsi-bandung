df35154e26255a0239616c60c5983471496b3347
irfansud2nd <irfansud2nd@gmail.com>
2024-01-23 20:27:03 +0700

    page title

--- app/event/[id]/juaraumum/page.tsx
+++ app/event/[id]/juaraumum/page.tsx
@@ -1,4 +1,19 @@
 import JuaraUmum from "@/components/event/juaraUmum/JuaraUmum";
+import { events } from "@/utils/event/eventConstants";
+import { Metadata } from "next";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { id: string };
+}): Promise<Metadata> {
+  const eventTitle =
+    events[events.findIndex((event) => event.id == params.id)].title;
+  return {
+    title: `Rekapitulasi Perolehan Medali ${eventTitle}`,
+    description: "Rekapitulasi Perolehan Medali - IPSI Kota Bandung",
+  };
+}
 
 const page = ({ params }: { params: { id: string } }) => {
   return <JuaraUmum eventId={params.id} />;
