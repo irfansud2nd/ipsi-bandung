36dca414d906df0a69ffd6bdfec6f1af38286255
irfansud2nd <irfansud2nd@gmail.com>
2024-02-05 12:20:32 +0700

    Move Side Menu

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,15 +1,15 @@
 "use client";
-import SideMenu from "@/components/admin/dashboard/SideMenu";
+import SideMenu from "@/components/admin/SideMenu";
 import IsAuthorized from "@/components/auth/IsAuthorized";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    // <IsAuthorized access="admin">
-    <div className="h-full w-full grid grid-cols-[auto_1fr] relative">
-      <SideMenu />
-      {children}
-    </div>
-    // </IsAuthorized>
+    <IsAuthorized access="admin">
+      <div className="h-full w-full grid grid-cols-[auto_1fr] relative">
+        <SideMenu />
+        {children}
+      </div>
+    </IsAuthorized>
   );
 };
 export default layout;
--- components/admin/Dashboard/AdminDashboard.tsx
+++ components/admin/Dashboard/AdminDashboard.tsx
@@ -1,4 +1,4 @@
-import SideMenu from "./SideMenu";
+import SideMenu from "../SideMenu";
 
 const AdminDashboard = () => {
   return <div className="h-[4000px]">Dashboard</div>;
