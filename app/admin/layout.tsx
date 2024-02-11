78c4f98b498ca170762e613ad8cf40df1208a79a
irfansud2nd <irfansud2nd@gmail.com>
2024-02-11 18:44:02 +0700

    fix mobile on admin

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -5,7 +5,7 @@ import IsAuthorized from "@/components/auth/IsAuthorized";
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <IsAuthorized access="admin">
-      <div className="h-full w-full sm:grid grid-cols-[auto_1fr] relative">
+      <div className="h-full w-full grid grid-cols-1 sm:grid-cols-[auto_1fr] relative">
         <SideMenu />
         {children}
       </div>
